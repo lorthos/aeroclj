8477f75cd06a62c2ddff33a01e3f8d2e9b4e0691
Halit Olali <halitolali@gmail.com>
2016-03-10 21:34:39 +0100

    0.1.1 release

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject aeroclj "0.1.1-SNAPSHOT"
+(defproject aeroclj "0.1.1"
   :description "Idiomatic Clojure wrapper around AeroSpike Java client."
   :url "https://github.com/codemomentum/aeroclj"
   :dependencies [[org.clojure/clojure "1.8.0"]
